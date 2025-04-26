25f59ba93c0692bad8277e0e865abcbccf08b301
carlos dias <carloseduardodc090@gmail.com>
2025-04-26 01:21:35 -0400

    chore: add prefix

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json(['message' => 'Hello world!']);
 });
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('jwt')->group(function () {
-    Route::get('/user', [AuthController::class, 'getUser']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::put('/user', [AuthController::class, 'updateUser']);
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    
+    Route::middleware('jwt')->group(function () {
+        Route::get('/user', [AuthController::class, 'getUser']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::put('/user', [AuthController::class, 'updateUser']);
+    });
 });
